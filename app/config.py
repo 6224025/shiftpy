e1aab87666423dfe489a46b3238995f81583bd74
6224025 <6224025@ed.tus.ac.jp>
2025-10-02 00:53:29 +0900

    good

--- app/config.py
+++ app/config.py
@@ -3,8 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     DATABASE_URL: str
     SECRET_KEY: str
-    APP_TITLE: str = "シフト管理アプリ"
-
+    APP_TITLE: str 
     model_config = SettingsConfigDict(env_file=".env", extra='ignore')
 
 settings = Settings()
